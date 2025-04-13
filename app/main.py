e290e1208904d1769a3d95354067fddb1bc393d2
Aleksey <aleksey-000@mail.ru>
2025-04-13 19:58:04 +0300

    включение маршрутизации для категорий товаров и товаров

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routers import category
+from app.routers import category, products
 
 app = FastAPI()
+
 
 @app.get('/')
 async def welcome() -> dict:
     return {'message': 'Welcome!'}
 
+
 app.include_router(category.router)
+app.include_router(products.router)

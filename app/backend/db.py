93bf102bc433905f2eabb4fd509b8a40cd01e529
Aleksey <aleksey-000@mail.ru>
2025-04-13 19:56:38 +0300

    создание базового класса модели базы данных

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine('sqlite:///ecommerce.db', echo=True)
 session_local = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass

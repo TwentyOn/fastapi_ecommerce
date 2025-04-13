fe295ce7c442813ff466cd1fcb09f73229c7e845
Aleksey <aleksey-000@mail.ru>
2025-04-13 19:51:30 +0300

    создание скелета маршрутов для отображения товаров

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,17 +1,33 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix='/products', tags='Продукты')
+router = APIRouter(prefix='/products', tags=['Продукты'])
+
 
 @router.get('/')
 async def get_all_products() -> dict:
     pass
 
+
 @router.post('/')
 async def create_product():
     pass
 
+
 @router.get('/{category_slug}')
-async def get_product_by_category(category_slug: str) -> str:
+async def get_product_by_category(category_slug: str) -> dict:
     pass
 
-router.get('/detail/{product_slug}')
+
+@router.get('/detail/{product_slug}')
+async def get_product(product_slug: str):
+    pass
+
+
+@router.put('update/{product_slug}')
+async def update_product_info(product_slug: str) -> str:
+    pass
+
+
+@router.delete('delete/{product_slug}')
+async def delete_product(product_slug: str) -> str:
+    pass

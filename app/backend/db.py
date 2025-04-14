5aa762d0bb96824cb13526ecb08ee189c909dbde
Aleksey <aleksey-000@mail.ru>
2025-04-14 21:29:26 +0300

    документирование кода

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine('sqlite:///ecommerce.db', echo=True)
-session_local = sessionmaker(bind=engine)
+engine = create_engine('sqlite:///ecommerce.db', echo=True) # движок sqlalchemy
+session_local = sessionmaker(bind=engine) # сессия базы данных для CRUD-функционала
+
 
 class Base(DeclarativeBase):
+    '''Базовый класс для создания моделей баз данных'''
     pass

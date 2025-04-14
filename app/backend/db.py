8ddb7c1d8983a85146b8fe81c4159d1775af53c3
Aleksey <aleksey-000@mail.ru>
2025-04-14 21:32:03 +0300

    приведение кода к PEP8

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine('sqlite:///ecommerce.db', echo=True) # движок sqlalchemy
-session_local = sessionmaker(bind=engine) # сессия базы данных для CRUD-функционала
+engine = create_engine('sqlite:///ecommerce.db', echo=True)  # движок sqlalchemy
+session_local = sessionmaker(bind=engine)  # сессия базы данных для CRUD-функционала
 
 
 class Base(DeclarativeBase):

6680c05344699914cc4241103a22c98201542de0
Aleksey <aleksey-000@mail.ru>
2025-04-15 14:01:53 +0300

    настройки БД

--- app/models/category.py
+++ app/models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 from app.backend.db import Base
@@ -10,5 +10,6 @@ class Category(Base):
     name = Column(String)
     slug = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
+    parent_id = Column(Integer, ForeignKey('categories.id'), nullable=True)
 
     products = relationship('Product', back_populates='category')
--- app/routers/category.py
+++ app/routers/category.py
@@ -1,18 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+from sqlalchemy import insert
+from app.backend.db_depends import get_db
+from app.schemas import CreateCategory, CreateProduct
+from app.models.category import Category
+from slugify import slugify
+
+from typing import Annotated
 
 router = APIRouter(prefix='/categories', tags=['Категории'])
 
-@router.get('/')
-async def get_all_categories() -> dict:
-    return {'all_categories': 'Все категории ёпта'}
 
-@router.post('/')
-async def create_category():
+@router.get('/{category_name}/{category_id}')
+async def get_all_categories() -> dict:
     pass
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+async def create_category(db: Annotated[Session, Depends(get_db)], create_category: CreateCategory) -> dict:
+    db.execute(insert(Category).values(name=create_category.name,
+                                       parent_id=create_category.parent_id,
+                                       slug=slugify(create_category.name)))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED,
+            'transaction': 'successful'}
+
 
 @router.put('/')
 async def update_category():
     pass
+
 
 @router.delete('/')
 async def delete_category():

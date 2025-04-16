a620c2fececda57eb1773c0d61e4b2c3627be930
Aleksey <aleksey-000@mail.ru>
2025-04-16 20:06:30 +0300

    смена ядра sqlite на асинхронный postgresql

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-engine = create_engine('sqlite:///ecommerce.db', echo=True)  # движок sqlalchemy
-session_local = sessionmaker(bind=engine)  # сессия базы данных для CRUD-функционала
+#engine = create_engine('sqlite:///ecommerce.db', echo=True)  # движок sqlite sqlalchemy
+#session_local = sessionmaker(bind=engine)  # фабрика сессий базы данных для CRUD-функционала
 
+engine = create_async_engine('postgresql+asyncpg://ecommerce:643941@localhost:5432/ecommerce', echo=True)
+async_sessions_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 class Base(DeclarativeBase):
     '''Базовый класс для создания моделей баз данных'''

d658c0c51132fdf7007434d395f6dc34b29b4da7
Aleksey <aleksey-000@mail.ru>
2025-06-23 00:33:34 +0300

    подключение асинхронного движка postgresql

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,9 +1,19 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine('postgresql+asyncpg://ecommerce:643941@localhost:5432/ecommerce', echo=True)
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+
+class Base(DeclarativeBase):
+    pass
+
+
+"""
+Движок и фабрика сессий на основе sqlite
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 engine = create_engine('sqlite:///ecommerce.db', echo=True)
 SessionLocal = sessionmaker(bind=engine)
-
-
-class Base(DeclarativeBase):
-    pass
+"""

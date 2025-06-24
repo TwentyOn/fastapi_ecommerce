1bc8ace46d72ae480944236c73dc2c00a96c3a96
Aleksey <aleksey-000@mail.ru>
2025-06-24 18:28:23 +0300

    внедрение ассинхронности для эндпоинтов категорий

--- app/routers/category.py
+++ app/routers/category.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy import insert, delete, select, update
 from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from slugify import slugify
 
 from typing import Annotated
@@ -13,43 +14,42 @@ router = APIRouter(prefix='/categories', tags=['категории'])
 
 
 @router.get('/all')
-async def get_all_categories(db_session: Annotated[Session, Depends(get_db)]):
-    all_categories = db_session.scalars(select(Category)).all()
-    return all_categories
+async def get_all_categories(db_session: Annotated[AsyncSession, Depends(get_db)]):
+    all_categories = await db_session.scalars(select(Category).where(Category.is_active))
+    return all_categories.all()
 
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def create_category(db_session: Annotated[Session, Depends(get_db)], category: CreateCategory) -> dict:
+async def create_category(db_session: Annotated[AsyncSession, Depends(get_db)], category: CreateCategory) -> dict:
     stmt = insert(Category).values(name=category.name, slug=slugify(category.name), parent_id=category.parent_id)
-    db_session.execute(stmt)
-    db_session.commit()
-    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+    await db_session.execute(stmt)
+    await db_session.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Успешно'}
 
 
 @router.put('/{category_slug}')
-async def update_category(db_session: Annotated[Session, Depends(get_db)], category_slug: str,
+async def update_category(db_session: Annotated[AsyncSession, Depends(get_db)], category_slug: str,
                           update_category: CreateCategory) -> dict:
-    category_to_update = db_session.scalar(select(Category)
-                                           .where(Category.slug == category_slug, Category.is_active == True))
+    category_to_update = await db_session.scalar(select(Category)
+                                                 .where(Category.slug == category_slug, Category.is_active == True))
+
     if not category_to_update:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Нет активных категорий с данным SLUG')
-    else:
-        db_session.execute(update(Category).where(Category.slug == category_slug, Category.is_active == True).values(
-            name=update_category.name,
-            slug=slugify(update_category.name),
-            parent_id=update_category.parent_id))
-        db_session.commit()
-        return {'status_code': status.HTTP_200_OK, 'message': 'Обновление успешно'}
+
+    category_to_update.name = update_category.name
+    category_to_update.slug = slugify(update_category.name)
+    category_to_update.parent_id = update_category.parent_id
+    await db_session.commit()
+    return {'status_code': status.HTTP_200_OK, 'message': 'Обновление успешно'}
 
 
 @router.delete('/{category_slug}')
-async def delete_category(db_session: Annotated[Session, Depends(get_db)], category_slug: str) -> str:
-    category_to_delete = db_session.scalar(
+async def delete_category(db_session: Annotated[Session, Depends(get_db)], category_slug: str) -> dict:
+    category_to_delete = await db_session.scalar(
         select(Category).where(Category.slug == category_slug, Category.is_active == True))
     if not category_to_delete:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Нет активных категорий с данным SLUG')
-    else:
-        db_session.execute(
-            update(Category).where(Category.slug == category_slug, Category.is_active == True).values(is_active=False))
-    db_session.commit()
-    return 'Категория успешно удалена'
+
+    category_to_delete.is_active = False
+    await db_session.commit()
+    return {'status_code': status.HTTP_200_OK, 'message': 'удаление успешно'}

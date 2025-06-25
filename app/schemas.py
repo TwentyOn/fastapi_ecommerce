97026a44ea4108f9d18cf42082bf19912595e881
Aleksey <aleksey-000@mail.ru>
2025-06-25 18:45:52 +0300

    Pydantic-модель создания пользователя

--- app/schemas.py
+++ app/schemas.py
@@ -13,3 +13,11 @@ class CreateProduct(BaseModel):
 class CreateCategory(BaseModel):
     name: str
     parent_id: int | None = Field(default=None, description='Вместо 0 должно быть null')
+
+
+class CreateUser(BaseModel):
+    name: str
+    lastname: str
+    username: str
+    email: str
+    password: str

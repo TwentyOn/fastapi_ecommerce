95a23d3604b5961e18566c3f9300b2f47fa770ec
Aleksey <aleksey-000@mail.ru>
2025-06-29 14:21:29 +0300

    Конечная точка для удаления пользователя

--- app/routers/permission.py
+++ app/routers/permission.py
@@ -8,7 +8,7 @@ from app.backend.db_depends import get_db
 from app.models.user import User
 from app.routers.auth import get_current_user
 
-router = APIRouter(prefix='/permission', tags=['permission'])
+router = APIRouter(prefix='/permission', tags=['Permission'])
 
 
 @router.patch('/')
@@ -30,4 +30,22 @@ async def supplier_permission(db_session: Annotated[AsyncSession, Depends(get_db
             await db_session.commit()
             return {'status_code': status.HTTP_200_OK, 'message': 'Пользователь теперь считается поставщиком'}
     else:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Вы не имеете прав администратора')
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Недостаточно прав')
+
+
+@router.delete('/delete')
+async def delete_user(db_session: Annotated[AsyncSession, Depends(get_db)],
+                      get_user: Annotated[dict, Depends(get_current_user)], user_id: int):
+    if get_user.get('is_admin'):
+        user = await db_session.scalar(select(User).where(User.id == user_id, User.is_active == True))
+
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Пользователя с таким id не существует')
+        elif user.is_admin:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Вы не можете удалить администратора')
+        else:
+            user.is_active = False
+            await db_session.commit()
+            return {'status_code': status.HTTP_200_OK, 'message': 'Пользователь успешно удалён'}
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Недостаточно прав')

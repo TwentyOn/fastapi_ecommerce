3d08be3fa2bc2e908cfcd63db6957785b740baa3
Aleksey <aleksey-000@mail.ru>
2025-07-04 16:07:08 +0300

    Параметры подключения к БД (sqlalchemy engine) теперь подтягиваются из файла переменных среды

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+import dotenv, os
 
-engine = create_async_engine('postgresql+asyncpg://ecommerce:643941@localhost:5432/ecommerce', echo=True)
+config = dotenv.dotenv_values('D://PycharmProjects/LearnFastAPI/app/migrations/.env')
+POSTGRES_USER = config["POSTGRES_USER"]
+POSTGRES_PASSWORD = config['POSTGRES_PASSWORD']
+POSTGRES_DB = config['POSTGRES_DB']
+
+engine = create_async_engine(f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}', echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
 
 class Base(DeclarativeBase):
     pass
